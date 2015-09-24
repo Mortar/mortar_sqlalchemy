3572539248403b269bfe234b362df481e6f27968
Chris Withers <chris@simplistix.co.uk>
2015-09-24 15:49:34 +0100

    the original tests for Common

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from mortar_mixins.common import Common
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
 from testfixtures import compare
 
 class TestCommon(TestCase):
@@ -52,3 +54,46 @@ class TestCommon(TestCase):
                 self.x = x
                 self._sa_instance_state = id(self)
         compare(repr(Foo(1)), 'Foo(x=1)')
+
+class MoreTests(TestCase):
+
+    def setUp(self):
+        class Model(Common, declarative_base()):
+            id =  Column(Integer, primary_key=True)
+        self.Model = Model
+
+    def test_table_name(self):
+        compare(self.Model.__table__.name, 'model')
+
+    def test_eq_wrong_type(self):
+        self.assertFalse(self.Model() == object())
+
+    def test_eq_different(self):
+        self.assertFalse(self.Model(id=1) == self.Model(id=2))
+
+    def test_eq_different_keys(self):
+        self.assertFalse(self.Model() == self.Model(id=2))
+
+    def test_eq_same(self):
+        self.assertTrue(self.Model(id=1) == self.Model(id=1))
+
+    def test_ne_wrong_type(self):
+        self.assertTrue(self.Model() != object())
+
+    def test_ne_different(self):
+        self.assertTrue(self.Model(id=1) != self.Model(id=2))
+
+    def test_ne_same(self):
+        self.assertTrue(self.Model(id=1) != self.Model(id=2))
+
+    def test_compare(self):
+        compare(
+            [self.Model(id=1), self.Model(id=2)],
+            [self.Model(id=1), self.Model(id=2)]
+            )
+
+    def test_repr(self):
+        compare('Model(id=1)', repr(self.Model(id=1)))
+
+    def test_str(self):
+        compare('Model(id=1)', str(self.Model(id=1)))
