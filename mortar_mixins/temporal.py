2869e9dca231c56472da2cdd90283fc87920a653
Chris Withers <chris@simplistix.co.uk>
2017-12-08 08:08:29 +0000

    set_for_period and helpers

--- mortar_mixins/temporal.py
+++ mortar_mixins/temporal.py
@@ -1,5 +1,7 @@
+from logging import getLogger, WARNING, INFO, DEBUG
+
 from psycopg2.extras import DateTimeRange
-from sqlalchemy import Column, CheckConstraint
+from sqlalchemy import Column, CheckConstraint, and_
 from sqlalchemy import Integer
 from sqlalchemy.dialects.postgresql import (
     ExcludeConstraint,
@@ -7,6 +9,11 @@ from sqlalchemy.dialects.postgresql import (
     )
 from sqlalchemy.event import listen
 from sqlalchemy.ext.declarative import has_inherited_table
+
+
+logger = getLogger(__name__)
+
+unset = object()
 
 
 class Temporal(object):
@@ -77,6 +84,157 @@ class Temporal(object):
         if self.value_to is None:
             return '%s onwards' % self.value_from
         return '%s to %s' % (self.value_from, self.value_to)
+
+    @property
+    def pretty_key(self):
+        return ', '.join('%s=%r' % (k, getattr(self, k))
+                         for k in self.key_columns)
+
+    @property
+    def pretty_value(self):
+        value = self.value_tuple
+        if len(value) == 1:
+            return value[0]
+        return ', '.join('%s=%r' % (k, getattr(self, k))
+                         for k in self.value_columns)
+
+    def starts_before(self, other):
+        return self.value_from is None or (
+            other.value_from is not None and self.value_from < other.value_from
+        )
+
+    def starts_before_or_at(self, other):
+        return self.value_from is None or (
+            other.value_from is not None and self.value_from <= other.value_from
+        )
+
+    def ends_after(self, other):
+        return other.value_to is not None and (
+                self.value_to is None or (self.value_to > other.value_to)
+        )
+
+    def period_compatible_with(self, other):
+        return self.period.lower == other.period.lower and (
+            self.period.upper is None or
+            self.period.upper==other.period.upper
+        )
+
+    def overlaps_filter(self):
+        cls = type(self)
+        # based on this instance:
+        return and_(*(getattr(cls, key) == getattr(self, key)
+                      for key in self.key_columns))
+
+    def set_for_period(self, session, coalesce=True,
+                       add_logging=INFO,
+                       change_logging=WARNING,
+                       delete_logging=WARNING,
+                       unchanged_logging=DEBUG):
+        create = True
+        old = unset
+        new_value = self.value_tuple
+        model = type(self)
+
+        for index, existing in enumerate(session.query(model).filter(
+            self.overlaps_filter()
+        ).filter(
+            model.period.overlaps(self.period)
+        ).order_by(
+            model.period
+        )):
+
+            if index:
+                add_logging = change_logging
+
+            existing_value = existing.value_tuple
+            value_same = existing_value == new_value
+
+            if coalesce and value_same:
+                if existing.starts_before_or_at(self):
+                    existing.value_to = latest_ending(self, existing)
+                    create = False
+                    obj = existing
+                    logger.log(add_logging,
+                               '%s now from %s',
+                               self.pretty_key,
+                               obj.period_str())
+                else:
+                    # XXX coalesce multiple gets it wrong as we set
+                    # self here? but existing is the one that will be used?
+                    self.value_to = latest_ending(self, existing)
+                    obj = self
+                    change_logging = add_logging
+                    session.delete(existing)
+                    session.flush()
+                    if index:
+                        logger.log(add_logging,
+                                   'deleted %s from %s, was set to %s',
+                                   existing.pretty_key,
+                                   existing.period_str(),
+                                   existing.pretty_value)
+                    else:
+                        old = existing
+
+            elif value_same and self.period_compatible_with(existing):
+                create = False
+                logger.log(unchanged_logging,
+                           '%s from %s left at %s',
+                           self.pretty_key,
+                           self.period_str(),
+                           self.pretty_value)
+            else:
+                if existing.starts_before(self):
+                    existing.value_to = self.value_from
+                else:
+                    if self.value_to is None and self.starts_before(existing):
+                        self.value_to = existing.value_from
+                        add_logging = change_logging
+                        break
+                    elif existing.ends_after(self):
+                        existing.value_from = self.value_to
+                        old = existing
+                    else:
+                        session.delete(existing)
+                        session.flush()
+                        if index:
+                            logger.log(delete_logging,
+                                       'deleted %s from %s, was set to %s',
+                                       existing.pretty_key,
+                                       existing.period_str(),
+                                       existing.pretty_value)
+                        else:
+                            old = existing
+
+        if create:
+            session.add(self)
+            if old is unset:
+                logger.log(add_logging,
+                           '%s from %s set to %s',
+                           self.pretty_key,
+                           self.period_str(),
+                           self.pretty_value)
+            else:
+                if old.value_tuple == self.value_tuple:
+                    logger.log(change_logging,
+                               '%s changed period from %s to %s',
+                               self.pretty_key,
+                               old.period_str(),
+                               self.period_str())
+                else:
+                    logger.log(change_logging,
+                               '%s from %s changed from %s to %s',
+                               self.pretty_key,
+                               self.period_str(),
+                               old.pretty_value,
+                               self.pretty_value)
+
+
+def latest_ending(x, other):
+    x_to = x.value_to
+    y_to = other.value_to
+    if x_to is None or y_to is None:
+        return None
+    return max(x_to, y_to)
 
 
 def add_constraints_and_attributes(mapper, class_):
