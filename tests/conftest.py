d57aa422a5411708733692e0d2990f5a638b1d6a
Chris Withers <chris@simplistix.co.uk>
2023-05-11 08:01:21 +0100

    Switch to testservices

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,23 @@
 import pytest
+from sqlalchemy import text
 from sqlalchemy.orm import declarative_base
+from testservices.provider import Provider
+from testservices.services.databases import PostgresContainer, Database, DatabaseFromEnvironment
 
 from mortar_sqlalchemy.testing import connection_in_transaction
 
 
 @pytest.fixture(scope='session')
 def db():
-    with connection_in_transaction() as conn:
-        yield conn
+    provider = Provider(
+        Database,
+        DatabaseFromEnvironment(timeout=300),
+        PostgresContainer(),
+    )
+    with provider as database:
+        with connection_in_transaction(database.url) as conn:
+            conn.execute(text('CREATE EXTENSION IF NOT EXISTS btree_gist'))
+            yield conn
 
 
 @pytest.fixture
