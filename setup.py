1bc1bd07f1ad85e3b42e07e6d4ce43121b259dc8
Chris Withers <chris@simplistix.co.uk>
2024-10-15 19:30:54 +0100

    Drop support for Python 3.10 and earlier

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3.11",
     install_requires=(
         'SQLAlchemy<2',
         'psycopg2',
